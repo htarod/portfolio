f6d40dd86c30b1be37bf28262c2687c2aa892123
Agatha Rodrigues <55159552+htarod@users.noreply.github.com>
2025-03-10 15:52:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const botaoWhatsApp = document.querySelector(".whatsapp-button");
-    
-    botaoWhatsApp.addEventListener("mouseover", function () {
-        botaoWhatsApp.style.backgroundColor = "#25D366"; // Cor verde do WhatsApp
-    });
-
-    botaoWhatsApp.addEventListener("mouseout", function () {
-        botaoWhatsApp.style.backgroundColor = "green"; // Volta ao verde original
-    });
-});
+function openWindow(page) {
+    window.location.href = page;
+}
